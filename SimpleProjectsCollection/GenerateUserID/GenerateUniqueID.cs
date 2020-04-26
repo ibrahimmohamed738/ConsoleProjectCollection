1ecf7fe15998620ef73db2196573a4610c9b91d7
ibrahim mohamed <ibrahim.mohamed738@outlook.com>
2020-04-26 10:37:40 +0300

    Adding GenerateIDs Project
    Adding IDs generation for UserId and Bill ID.

---
+++ SimpleProjectsCollection/GenerateUserID/GenerateUniqueID.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenerateUserID
+{
+    public class GenerateUniqueID
+    {
+
+        public string GenerateUserID ()
+        {
+            string UserId = "UR" + DateTime.Now.ToString("yyMMddhhmmssfffff");
+            return UserId;
+            //return Guid.NewGuid().ToString("N");
+        }
+
+        public string GenerateBillID()
+        {
+            string BillId = "BL" + DateTime.Now.ToString("yyMMddhhmmssfffff");
+            return BillId;
+            //return Guid.NewGuid().ToString("N");
+        }
+    }
+}
---
+++ SimpleProjectsCollection/GenerateUserID/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenerateUserID
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var GetID = new GenerateUniqueID();
+          
+            Console.WriteLine("Your UserID : {0}", GetID.GenerateUserID());
+          
+            Console.WriteLine("Your BillNo : {0}", GetID.GenerateBillID());
+            Console.ReadLine();
+        }
+    }
+}
